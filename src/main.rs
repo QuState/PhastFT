a8dae300e5199214dec46665613a826621aa6573
Saveliy Yusufov <sy2685@columbia.edu>
2024-01-16 20:40:34 -0500

    Fix for prebuilt twiddles and portable SIMD

--- src/main.rs
+++ src/main.rs
@@ -57,13 +57,47 @@ fn bit_reverse_permutation<T>(buf: &mut [T]) {
     }
 }
 
+fn fft_chunk_n_simd(state: &mut State, twiddles_re: &[Float], twiddles_im: &[Float], dist: usize) {
+    let chunk_size = dist << 1;
+    assert!(chunk_size >= 16);
+
+    state
+        .reals
+        .chunks_exact_mut(chunk_size)
+        .zip(state.imags.chunks_exact_mut(chunk_size))
+        .for_each(|(reals_chunk, imags_chunk)| {
+            let (reals_s0, reals_s1) = reals_chunk.split_at_mut(dist);
+            let (imags_s0, imags_s1) = imags_chunk.split_at_mut(dist);
+
+            reals_s0
+                .chunks_exact_mut(8)
+                .zip(reals_s1.chunks_exact_mut(8))
+                .zip(imags_s0.chunks_exact_mut(8))
+                .zip(imags_s1.chunks_exact_mut(8))
+                .zip(twiddles_re.chunks_exact(8))
+                .zip(twiddles_im.chunks_exact(8))
+                .for_each(|(((((re_s0, re_s1), im_s0), im_s1), w_re), w_im)| {
+                    let real_c0 = f64x8::from_slice(re_s0);
+                    let real_c1 = f64x8::from_slice(re_s1);
+                    let imag_c0 = f64x8::from_slice(im_s0);
+                    let imag_c1 = f64x8::from_slice(im_s1);
+
+                    let twiddles_re = f64x8::from_slice(w_re);
+                    let twiddles_im = f64x8::from_slice(w_im);
+
+                    re_s0.copy_from_slice((real_c0 + real_c1).as_array());
+                    im_s0.copy_from_slice((imag_c0 + imag_c1).as_array());
+                    let v_re = real_c0 - real_c1;
+                    let v_im = imag_c0 - imag_c1;
+                    re_s1.copy_from_slice((v_re * twiddles_re - v_im * twiddles_im).as_array());
+                    im_s1.copy_from_slice((v_re * twiddles_im + v_im * twiddles_re).as_array());
+                });
+        });
+}
+
 // TODO(saveliy): parallelize
 fn fft_chunk_n(state: &mut State, twiddles_re: &[Float], twiddles_im: &[Float], dist: usize) {
-    // assert_eq!(twiddles_im.len(), twiddles_re.len());
-    // assert_eq!(state.len() >> 1, twiddles_re.len());
-
     let chunk_size = dist << 1;
-    let step = twiddles_im.len() / dist;
 
     state
         .reals
@@ -78,8 +112,8 @@ fn fft_chunk_n(state: &mut State, twiddles_re: &[Float], twiddles_im: &[Float], 
                 .zip(reals_s1.iter_mut())
                 .zip(imags_s0.iter_mut())
                 .zip(imags_s1.iter_mut())
-                .zip(twiddles_re.iter().step_by(step))
-                .zip(twiddles_im.iter().step_by(step))
+                .zip(twiddles_re.iter())
+                .zip(twiddles_im.iter())
                 .for_each(|(((((re_s0, re_s1), im_s0), im_s1), w_re), w_im)| {
                     let real_c0 = *re_s0;
                     let real_c1 = *re_s1;
@@ -178,6 +212,9 @@ pub fn fft_dif(state: &mut State) {
         let dist = 1 << t;
         let chunk_size = dist << 1;
 
+        if chunk_size > 16 {
+            fft_chunk_n_simd(state, &twiddles_re, &twiddles_im, dist);
+        }
         if chunk_size == 2 {
             fft_chunk_2(state);
         } else if chunk_size == 4 {
@@ -211,8 +248,8 @@ fn bm_fft(num_qubits: usize) {
     for i in 4..num_qubits {
         println!("run PhastFT with {i} qubits");
         let n = 1 << i;
-        let x_re: Vec<Float> = (1..n + 1).map(|i| i as Float).collect();
-        let x_im: Vec<Float> = (1..n + 1).map(|i| i as Float).collect();
+        let x_re: Vec<Float> = (1..=n).map(|i| i as Float).collect();
+        let x_im: Vec<Float> = (1..=n).map(|i| i as Float).collect();
         let mut state = State {
             reals: x_re,
             imags: x_im,
