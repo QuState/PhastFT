d585aac2c70255ecb0548374c293ff9cfbb2e795
Saveliy Yusufov <sy2685@columbia.edu>
2024-01-29 13:57:46 -0500

    Use numpy crate to avoid overhead in pybindings

--- pybindings/src/lib.rs
+++ pybindings/src/lib.rs
@@ -1,26 +1,10 @@
-use num_complex::Complex;
+use numpy::PyReadwriteArray1;
 use phastft::fft_dif;
 use pyo3::prelude::*;
-use spinoza::core::State;
-use spinoza::math::Float;
 
 #[pyfunction]
-fn fft(reals: Vec<Float>, imags: Vec<Float>) -> (Vec<Float>, Vec<Float>) {
-    let n = reals.len().ilog2() as u8;
-
-    // println!("reals: {reals:?}");
-    // println!("imags: {imags:?}");
-
-    let mut state = State { reals, imags, n };
-
-    let now = std::time::Instant::now();
-    fft_dif(&mut state);
-    let elapsed = now.elapsed().as_micros();
-    println!("pure rust PhastFT call: {elapsed} us");
-
-    // println!("{state}");
-
-    (state.reals, state.imags)
+fn fft(mut reals: PyReadwriteArray1<f64>, mut imags: PyReadwriteArray1<f64>) {
+    fft_dif(reals.as_slice_mut().unwrap(), imags.as_slice_mut().unwrap());
 }
 
 /// A Python module implemented in Rust.
