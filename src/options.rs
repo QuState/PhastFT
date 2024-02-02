db724700dadf89075313d17f16e07ac6c392bf91
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-02-02 22:23:06 +0000

    Simplify options down to a single multi-threading knob

--- src/options.rs
+++ src/options.rs
@@ -8,7 +8,10 @@
 #[non_exhaustive]
 #[derive(Debug, Clone, Default)]
 pub struct Options {
-    pub bit_reverse: BitReverseAlgorithm,
+    /// Whether to run bit reversal step in 2 threads instead of one.
+    /// This is beneficial only at large input sizes (i.e. gigabytes of data).
+    /// The exact threshold where it starts being beneficial varies depending on the hardware.
+    pub multithreaded_bit_reversal: bool,
 }
 
 impl Options {
@@ -16,28 +19,10 @@ impl Options {
         let mut options = Options::default();
         let n: usize = input_size.ilog2() as usize;
         if n < 22 {
-            options.bit_reverse = BitReverseAlgorithm::Cobra;
+            options.multithreaded_bit_reversal = false;
         } else {
-            options.bit_reverse = BitReverseAlgorithm::MultiThreadedCobra;
+            options.multithreaded_bit_reversal = true;
         }
         options
     }
-}
-
-/// The algorithm to use for bit reversal.
-/// Different algorithms perform best on different input sizes.
-#[derive(Debug, Copy, Clone, PartialEq, Default)]
-pub enum BitReverseAlgorithm {
-    #[default]
-    /// Straightforward algorithm that performs best at smaller sizes
-    Plain,
-    /// Cache-Optimal Bit Reversal Algorithm
-    ///
-    /// This is faster at larger datasets that do not fit into the cache.
-    /// The exact threshold where it starts being beneficial varies depending on the hardware.
-    Cobra,
-    /// COBRA but run on two threads instead of one.
-    /// Typically beneficial at even larger sizes than single-threaded COBRA, and slower otherwise.
-    /// The exact threshold where it starts being beneficial varies depending on the hardware.
-    MultiThreadedCobra,
 }
