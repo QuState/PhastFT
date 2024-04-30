21d77dbf52883bcdf48d474f4856e8640bb8d6f4
Saveliy Yusufov <sy2685@columbia.edu>
2024-04-30 12:05:30 -0400

    Add a function to separate AoS to SoA

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,9 @@
 #![forbid(unsafe_code)]
 #![feature(portable_simd, avx512_target_feature)]
 
+use num_complex::Complex;
+use num_traits::Float;
+
 use crate::cobra::cobra_apply;
 use crate::kernels::{
     fft_32_chunk_n_simd, fft_64_chunk_n_simd, fft_chunk_2, fft_chunk_4, fft_chunk_n,
@@ -156,8 +159,8 @@ mod tests {
     use std::ops::Range;
 
     use utilities::assert_float_closeness;
-    use utilities::rustfft::num_complex::Complex;
     use utilities::rustfft::FftPlanner;
+    use utilities::rustfft::num_complex::Complex;
 
     use super::*;
 
@@ -256,4 +259,29 @@ mod tests {
 
     test_fft_correctness!(fft_correctness_32, f32, fft_32, 4, 9);
     test_fft_correctness!(fft_correctness_64, f64, fft_64, 4, 17);
+}
+
+/// Utility function to separate interleaved format signals (i.e., Vector of Complex Number Structs)
+/// into separate vectors for the corresponding real and imaginary components.
+pub fn separate_re_im<T: Float>(signal: &[Complex<T>], chunk_size: usize) -> (Vec<T>, Vec<T>) {
+    let mut reals = Vec::with_capacity(signal.len());
+    let mut imaginaries = Vec::with_capacity(signal.len());
+    let iter = signal.chunks_exact(chunk_size);
+    let rem = iter.remainder();
+
+    // We don't assume power of 2 size, so we don't use `chunks_exact`.
+    // Process each chunk, including the last chunk which may be smaller.
+    for chunk in iter {
+        for num in chunk {
+            reals.push(num.re);
+            imaginaries.push(num.im);
+        }
+    }
+
+    for num in rem {
+        reals.push(num.re);
+        imaginaries.push(num.im);
+    }
+
+    (reals, imaginaries)
 }
