b8affb2cf0ec401d0ba0a90aabd558a253b433a1
Saveliy Yusufov <sy2685@columbia.edu>
2024-05-22 16:19:11 -0400

    Account for different signal sizes

--- benches/bench.rs
+++ benches/bench.rs
@@ -3,7 +3,7 @@
 use std::simd::{f32x16, f64x8};
 
 use bytemuck::cast_slice;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use num_complex::Complex;
 
 use phastft::separate_re_im_f64;
@@ -44,14 +44,13 @@ macro_rules! impl_separate_re_im_with_capacity {
                 imags.extend_from_slice(&im_deinterleaved.to_array());
             }
 
-    let remainder = complex_t.chunks_exact(CHUNK_SIZE).remainder();
-    if !remainder.is_empty() {
-        let i = n - remainder.len() / 2;
-        for (j, chunk) in remainder.chunks_exact(2).enumerate() {
-            reals.push(chunk[0]);
-            imags.push(chunk[1]);
-        }
-    }
+            let remainder = complex_t.chunks_exact(CHUNK_SIZE).remainder();
+            if !remainder.is_empty() {
+                for chunk in remainder.chunks_exact(2) {
+                    reals.push(chunk[0]);
+                    imags.push(chunk[1]);
+                }
+            }
 
             (reals, imags)
         }
@@ -65,17 +64,27 @@ impl_separate_re_im_with_capacity!(separate_re_im_f32_with_cap, f32, 16, f32x16)
 impl_separate_re_im_with_capacity!(separate_re_im_f64_with_cap, f64, 8, f64x8);
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let signal: Vec<Complex<f64>> = (0..(1 << 20))
-        .map(|x| Complex::new(x as f64, (x * 2) as f64))
-        .collect();
+    let sizes = vec![1 << 10, 1 << 12, 1 << 14, 1 << 16, 1 << 18, 1 << 20];
 
-    c.bench_function("separate_re_im_with_zeroed_vecs", |b| {
-        b.iter(|| separate_re_im_f64(black_box(&signal)));
-    });
+    let mut group = c.benchmark_group("separate_re_im");
+    for size in sizes.into_iter() {
+        let signal: Vec<_> = (0..size)
+            .map(|x| Complex::new(x as f64, (x * 2) as f64))
+            .collect();
 
-    c.bench_function("separate_re_im_with_capacity", |b| {
-        b.iter(|| separate_re_im_f64_with_cap(black_box(&signal)));
-    });
+        group.throughput(Throughput::Elements(size));
+
+        group.bench_with_input(
+            BenchmarkId::new("with_zeroed_vecs", size),
+            &signal,
+            |b, s| b.iter(|| separate_re_im_f64(black_box(s))),
+        );
+
+        group.bench_with_input(BenchmarkId::new("with_capacity", size), &signal, |b, s| {
+            b.iter(|| separate_re_im_f64_with_cap(black_box(s)));
+        });
+    }
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
