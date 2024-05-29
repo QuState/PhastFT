22a7f1d2f8e88da483235683b4b39aa511583e31
Saveliy Yusufov <sy2685@columbia.edu>
2024-05-29 11:23:03 -0400

    Implement r2c test macro for f32 and f64

--- src/fft.rs
+++ src/fft.rs
@@ -1,27 +1,28 @@
 //! Implementation of Real valued FFT
-use std::f64::consts::PI;
+use num_traits::Float;
 
 use crate::fft_64;
-use crate::{compute_twiddle_factors, Direction};
+use crate::Direction;
 
-fn precompute_chirp(n: usize, m: usize) -> (Vec<f64>, Vec<f64>) {
-    let mut chirp_re = vec![0.0; m];
-    let mut chirp_im = vec![0.0; m];
-    for k in 0..n {
-        let angle = PI * (k * k) as f64 / n as f64;
-        chirp_re[k] = angle.cos();
-        chirp_im[k] = -angle.sin();
+fn compute_twiddle_factors<T: Float>(big_n: usize) -> (Vec<T>, Vec<T>) {
+    let half_n = big_n / 2;
+    let mut real_parts = Vec::with_capacity(half_n);
+    let mut imag_parts = Vec::with_capacity(half_n);
+    let two: T = T::from(2.0).unwrap();
+    let pi: T = T::from(std::f64::consts::PI).unwrap();
+
+    for k in 0..half_n {
+        let angle = -two * pi * T::from(k).unwrap() / T::from(big_n).unwrap();
+        real_parts.push(angle.cos());
+        imag_parts.push(angle.sin());
     }
-    for k in n..m {
-        chirp_re[k] = 0.0;
-        chirp_im[k] = 0.0;
-    }
-    (chirp_re, chirp_im)
+
+    (real_parts, imag_parts)
 }
 
 /// Implementation of Real-Valued FFT
-pub fn real_fft(input_re: &[f64]) -> (Vec<f64>, Vec<f64>) {
-    let n = input_re.len();
+pub fn r2c_fft_f64(input_re: &[f64]) -> (Vec<f64>, Vec<f64>) {
+    let big_n = input_re.len();
 
     // Splitting odd and even
     let (mut z_even, mut z_odd): (Vec<_>, Vec<_>) =
@@ -45,20 +46,6 @@ pub fn real_fft(input_re: &[f64]) -> (Vec<f64>, Vec<f64>) {
         *zo = -(*zo);
     }
 
-    println!("Z even: {z_even:?}\nZ odd: {z_odd:?}\n");
-    println!("Z even min conj : {z_even_min_conj:?}\nZ odd min conj: {z_odd_min_conj:?}");
-
-    /*
-    [ 64.       +72.j        -27.3137085+11.3137085j -16.        +0.j
-    -11.3137085 -4.6862915j  -8.        -8.j         -4.6862915-11.3137085j
-       0.       -16.j         11.3137085-27.3137085j]
-
-    [ 64.       -72.j         11.3137085+27.3137085j   0.       +16.j
-      -4.6862915+11.3137085j  -8.        +8.j        -11.3137085 +4.6862915j
-     -16.        -0.j        -27.3137085-11.3137085j]
-
-     */
-
     // Zx = 0.5  * (Z + Zminconj)
     let (z_x_re, z_x_im): (Vec<_>, Vec<_>) = z_even
         .iter()
@@ -72,8 +59,6 @@ pub fn real_fft(input_re: &[f64]) -> (Vec<f64>, Vec<f64>) {
         })
         .unzip();
 
-    println!("Zx reals: {z_x_re:?}\nZx imags: {z_x_im:?}\n");
-
     // Zy = -0.5j * (Z - Zminconj)
     let (z_y_re, z_y_im): (Vec<_>, Vec<_>) = z_even
         .iter()
@@ -84,32 +69,20 @@ pub fn real_fft(input_re: &[f64]) -> (Vec<f64>, Vec<f64>) {
             let a = ze - ze_mc;
             let b = zo - zo_mc;
 
-            // -0.5i (a + ib) = -0.5i * a + 0.5 * b
+            // -0.5i (a + ib) = -0.5i * a - 0.5i * ib
+            //                = -0.5i * a + 0.5 * b
+            //                = 0.5 * b - 0.5 * ia
             (0.5 * b, -0.5 * a)
         })
         .unzip();
 
-    /*
-    Zx:
-
-    [64. +0.j        -8.+19.3137085j -8. +8.j        -8. +3.3137085j
-     -8. +0.j        -8. -3.3137085j -8. -8.j        -8.-19.3137085j]
-
-    Zy:
-
-    [72. -0.j        -8.+19.3137085j -8. +8.j        -8. +3.3137085j
-     -8. +0.j        -8. -3.3137085j -8. -8.j        -8.-19.3137085j]
-     */
-
-    println!("Zy reals: {z_y_re:?}\nZy imags: {z_y_im:?}");
-
-    let (twiddle_re, twiddle_im) = compute_twiddle_factors(n); // np.exp(-1j * 2 * math.pi * np.arange(N//2) / N)
+    let (twiddle_re, twiddle_im): (Vec<f64>, Vec<f64>) = compute_twiddle_factors(big_n);
 
     // Zall = np.concatenate([Zx + W*Zy, Zx - W*Zy])
-    let mut z_all_re = Vec::new();
-    let mut z_all_im = Vec::new();
+    let mut z_all_re = Vec::with_capacity(z_x_re.len() + z_y_re.len());
+    let mut z_all_im = Vec::with_capacity(z_x_im.len() + z_y_im.len());
 
-    for i in 0..n / 2 {
+    for i in 0..big_n / 2 {
         let zx_re = z_x_re[i];
         let zx_im = z_x_im[i];
         let zy_re = z_y_re[i];
@@ -125,7 +98,7 @@ pub fn real_fft(input_re: &[f64]) -> (Vec<f64>, Vec<f64>) {
         z_all_im.push(zx_im + wz_im);
     }
 
-    for i in 0..n / 2 {
+    for i in 0..big_n / 2 {
         let zx_re = z_x_re[i];
         let zx_im = z_x_im[i];
         let zy_re = z_y_re[i];
@@ -141,6 +114,14 @@ pub fn real_fft(input_re: &[f64]) -> (Vec<f64>, Vec<f64>) {
         z_all_im.push(zx_im - wz_im);
     }
 
+    // println!(
+    //     "z_x_re: {}\nz_x_im: {}\nz_all_re len: {}\nz_all_im len: {}\n",
+    //     z_x_re.len(),
+    //     z_x_im.len(),
+    //     z_all_re.len(),
+    //     z_all_im.len()
+    // );
+
     // return Zall
     (z_all_re, z_all_im)
 }
@@ -151,26 +132,34 @@ mod tests {
 
     use super::*;
 
-    #[test]
-    fn r2c_vs_c2c() {
-        let n = 4;
-        let big_n = 1 << 4;
-        let mut input_re: Vec<_> = (1..=big_n).map(|i| i as f64).collect(); // Length is 7, which is a prime number
+    macro_rules! impl_r2c_vs_c2c_test {
+        ($func:ident, $precision:ty, $fft_precision:ident, $rftt_funct:ident) => {
+            #[test]
+            fn $func() {
+                for n in 4..=10 {
+                    let big_n = 1 << n;
+                    let input_re: Vec<$precision> = (1..=big_n).map(|i| i as $precision).collect(); // Length is 7, which is a prime number
 
-        let (r2c_res_reals, r2c_res_imags) = real_fft(&mut input_re);
+                    let (r2c_res_reals, r2c_res_imags) = $rftt_funct(&input_re);
 
-        input_re = (1..=big_n).map(|i| i as f64).collect();
-        let mut input_im = vec![0.0; input_re.len()]; // Assume the imaginary part is zero for this example
-        fft_64(&mut input_re, &mut input_im, Direction::Forward);
+                    let mut input_re: Vec<_> = (1..=big_n).map(|i| i as $precision).collect();
+                    let mut input_im = vec![0.0; input_re.len()]; // Assume the imaginary part is zero for this example
+                    $fft_precision(&mut input_re, &mut input_im, Direction::Forward);
 
-        r2c_res_reals
-            .iter()
-            .zip(r2c_res_imags.iter())
-            .zip(input_re.iter())
-            .zip(input_im.iter())
-            .for_each(|(((a_re, a_im), e_re), e_im)| {
-                assert_float_closeness(*a_re, *e_re, 1e-6);
-                assert_float_closeness(*a_im, *e_im, 1e-6);
-            });
+                    r2c_res_reals
+                        .iter()
+                        .zip(r2c_res_imags.iter())
+                        .zip(input_re.iter())
+                        .zip(input_im.iter())
+                        .for_each(|(((a_re, a_im), e_re), e_im)| {
+                            assert_float_closeness(*a_re, *e_re, 1e-6);
+                            assert_float_closeness(*a_im, *e_im, 1e-6);
+                        });
+                }
+            }
+        };
     }
+
+    impl_r2c_vs_c2c_test!(r2c_vs_c2c_f64, f64, fft_64, r2c_fft_f64);
+    // impl_r2c_vs_c2c_test!(r2c_vs_c2c_f32, fft_32, r2c_fft_f32);
 }
