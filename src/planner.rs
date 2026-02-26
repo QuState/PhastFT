ac3d51f8517a88626a47166a4655772bb3ac4abe
Saveliy Yusufov <sy2685@columbia.edu>
2026-02-26 12:30:13 -0500

    chore: Remove DIF and COBRA implementations

--- src/planner.rs
+++ src/planner.rs
@@ -2,7 +2,6 @@
 //! a Fast Fourier Transform (FFT). Currently, the planner is responsible for
 //! pre-computing twiddle factors based on the input signal length, as well as the
 //! direction of the FFT.
-use crate::twiddles::{generate_twiddles, generate_twiddles_simd_32, generate_twiddles_simd_64};
 
 /// Reverse is for running the Inverse Fast Fourier Transform (IFFT)
 /// Forward is for running the regular FFT
@@ -13,68 +12,6 @@ pub enum Direction {
     /// Multiply the exponent term in the twiddle factor by -1
     Reverse = -1,
 }
-
-macro_rules! impl_planner_for {
-    ($struct_name:ident, $precision:ident, $generate_twiddles_simd_fn:ident) => {
-        /// The planner is responsible for pre-computing and storing twiddle factors for all the
-        /// `log_2(N)` stages of the FFT.
-        /// The amount of twiddle factors should always be a power of 2. In addition,
-        /// the amount of twiddle factors should always be `(1/2) * N`
-        pub struct $struct_name {
-            /// The real components of the twiddle factors
-            pub twiddles_re: Vec<$precision>,
-            /// The imaginary components of the twiddle factors
-            pub twiddles_im: Vec<$precision>,
-            /// The direction of the FFT associated with this `Planner`
-            pub direction: Direction,
-        }
-        impl $struct_name {
-            /// Create a `Planner` for an FFT of size `num_points`.
-            /// The twiddle factors are pre-computed based on the provided [`Direction`].
-            /// For `Forward`, use [`Direction::Forward`].
-            /// For `Reverse`, use [`Direction::Reverse`].
-            ///
-            /// # Panics
-            ///
-            /// Panics if `num_points < 1` or if `num_points` is __not__ a power of 2.
-            pub fn new(num_points: usize, direction: Direction) -> Self {
-                assert!(num_points > 0 && num_points.is_power_of_two());
-
-                if num_points <= 4 {
-                    return Self {
-                        twiddles_re: vec![],
-                        twiddles_im: vec![],
-                        direction,
-                    };
-                }
-
-                let dist = num_points >> 1;
-
-                let (twiddles_re, twiddles_im) = if dist >= 8 * 2 {
-                    $generate_twiddles_simd_fn(dist, Direction::Forward)
-                } else {
-                    generate_twiddles(dist, Direction::Forward)
-                };
-
-                assert_eq!(twiddles_re.len(), twiddles_im.len());
-
-                Self {
-                    twiddles_re,
-                    twiddles_im,
-                    direction,
-                }
-            }
-
-            pub(crate) fn num_twiddles(&self) -> usize {
-                assert_eq!(self.twiddles_re.len(), self.twiddles_im.len());
-                self.twiddles_re.len()
-            }
-        }
-    };
-}
-
-impl_planner_for!(Planner64, f64, generate_twiddles_simd_64);
-impl_planner_for!(Planner32, f32, generate_twiddles_simd_32);
 
 macro_rules! impl_planner_dit_for {
     ($struct_name:ident, $precision:ident) => {
@@ -136,23 +73,3 @@ macro_rules! impl_planner_dit_for {
 
 impl_planner_dit_for!(PlannerDit64, f64);
 impl_planner_dit_for!(PlannerDit32, f32);
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    macro_rules! test_no_twiddles {
-        ($test_name:ident, $planner:ty) => {
-            #[test]
-            fn $test_name() {
-                for num_points in [2, 4] {
-                    let planner = <$planner>::new(num_points, Direction::Forward);
-                    assert!(planner.twiddles_im.is_empty() && planner.twiddles_re.is_empty());
-                }
-            }
-        };
-    }
-
-    test_no_twiddles!(no_twiddles_64, Planner64);
-    test_no_twiddles!(no_twiddles_32, Planner32);
-}
