1e8a28354b9ea7c6181db5eed989ebfcde816c52
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2026-03-23 12:11:34 +0000

    Add precision configuration to benchmark scripts for rustfft and fftw

--- examples/fftwrb.rs
+++ examples/fftwrb.rs
@@ -3,12 +3,47 @@ use std::ptr::slice_from_raw_parts_mut;
 use std::str::FromStr;
 
 use fftw::array::AlignedVec;
-use fftw::plan::{C2CPlan, C2CPlan64};
+use fftw::plan::{C2CPlan, C2CPlan32, C2CPlan64};
 use fftw::types::{Flag, Sign};
-use utilities::gen_random_signal_f64;
 use utilities::rustfft::num_complex::Complex;
+use utilities::{gen_random_signal_f32, gen_random_signal_f64};
 
-fn benchmark_fftw(n: usize, iterations: usize) {
+fn benchmark_fftw_32(n: usize, iterations: usize) {
+    let big_n = 1 << n; // 2.pow(n)
+
+    let mut reals = vec![0.0f32; big_n];
+    let mut imags = vec![0.0f32; big_n];
+
+    gen_random_signal_f32(&mut reals, &mut imags);
+    let mut nums = AlignedVec::new(big_n);
+    reals
+        .drain(..)
+        .zip(imags.drain(..))
+        .zip(nums.iter_mut())
+        .for_each(|((re, im), z)| *z = Complex::new(re, im));
+
+    let mut plan = C2CPlan32::aligned(
+        &[big_n],
+        Sign::Backward,
+        Flag::DESTROYINPUT | Flag::ESTIMATE,
+    )
+    .unwrap();
+
+    let now = std::time::Instant::now();
+    for _ in 0..iterations {
+        plan.c2c(
+            // SAFETY: See above comment.
+            unsafe { &mut *slice_from_raw_parts_mut(nums.as_mut_ptr(), big_n) },
+            &mut nums,
+        )
+        .unwrap();
+    }
+    let elapsed = now.elapsed().as_nanos();
+    let elapsed_per_iteration = elapsed / iterations as u128;
+    println!("{elapsed_per_iteration}");
+}
+
+fn benchmark_fftw_64(n: usize, iterations: usize) {
     let big_n = 1 << n; // 2.pow(n)
 
     let mut reals = vec![0.0; big_n];
@@ -45,10 +80,18 @@ fn benchmark_fftw(n: usize, iterations: usize) {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    assert!(args.len() >= 3, "Usage {} <n> <iterations>", args[0]);
+    assert!(
+        args.len() >= 4,
+        "Usage {} <32|64> <n> <iterations>",
+        args[0]
+    );
 
-    let n = usize::from_str(&args[1]).unwrap();
-    let iterations = usize::from_str(&args[2]).unwrap();
+    let n = usize::from_str(&args[2]).unwrap();
+    let iterations = usize::from_str(&args[3]).unwrap();
 
-    benchmark_fftw(n, iterations);
+    match args[1].as_str() {
+        "32" => benchmark_fftw_32(n, iterations),
+        "64" => benchmark_fftw_64(n, iterations),
+        other => panic!("Invalid precision: {other}. Please pass 32 or 64"),
+    }
 }
--- examples/rustfft.rs
+++ examples/rustfft.rs
@@ -1,12 +1,42 @@
 use std::env;
 use std::str::FromStr;
 
-use utilities::gen_random_signal_f64;
-use utilities::rustfft::num_complex::Complex64;
+use utilities::rustfft::num_complex::{Complex32, Complex64};
 use utilities::rustfft::num_traits::Zero;
 use utilities::rustfft::FftPlanner;
+use utilities::{gen_random_signal_f32, gen_random_signal_f64};
 
-fn benchmark_rustfft(n: usize, iterations: usize) {
+fn benchmark_rustfft_32(n: usize, iterations: usize) {
+    let big_n = 1 << n; // 2.pow(n)
+
+    let mut reals = vec![0.0f32; big_n];
+    let mut imags = vec![0.0f32; big_n];
+
+    gen_random_signal_f32(&mut reals, &mut imags);
+    let mut signal = vec![Complex32::default(); big_n];
+    reals
+        .drain(..)
+        .zip(imags.drain(..))
+        .zip(signal.iter_mut())
+        .for_each(|((re, im), z)| {
+            z.re = re;
+            z.im = im;
+        });
+
+    let mut planner = FftPlanner::<f32>::new();
+    let fft = planner.plan_fft_forward(signal.len());
+    let mut scratch = vec![Complex32::zero(); fft.get_inplace_scratch_len()];
+
+    let now = std::time::Instant::now();
+    for _ in 0..iterations {
+        fft.process_with_scratch(&mut signal, scratch.as_mut_slice());
+    }
+    let elapsed = now.elapsed().as_nanos();
+    let elapsed_per_iteration = elapsed / iterations as u128;
+    println!("{elapsed_per_iteration}");
+}
+
+fn benchmark_rustfft_64(n: usize, iterations: usize) {
     let big_n = 1 << n; // 2.pow(n)
 
     let mut reals = vec![0.0f64; big_n];
@@ -38,10 +68,18 @@ fn benchmark_rustfft(n: usize, iterations: usize) {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    assert!(args.len() >= 3, "Usage {} <n> <iterations>", args[0]);
+    assert!(
+        args.len() >= 4,
+        "Usage {} <32|64> <n> <iterations>",
+        args[0]
+    );
 
-    let n = usize::from_str(&args[1]).unwrap();
-    let iterations = usize::from_str(&args[2]).unwrap();
+    let n = usize::from_str(&args[2]).unwrap();
+    let iterations = usize::from_str(&args[3]).unwrap();
 
-    benchmark_rustfft(n, iterations);
+    match args[1].as_str() {
+        "32" => benchmark_rustfft_32(n, iterations),
+        "64" => benchmark_rustfft_64(n, iterations),
+        other => panic!("Invalid precision: {other}. Please pass 32 or 64"),
+    }
 }
