65fd88c9b72c561178c2928c42c2286ff8f365e9
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2026-03-24 13:35:39 +0000

    PoC: parallelize stage 2 of in-place interleaving

--- src/complex_nums.rs
+++ src/complex_nums.rs
@@ -133,7 +133,7 @@ fn is_cycle_leader(val: usize, k: u32) -> bool {
 
 /// Performs an in-place interleaving of `[Reals | Imags]` into `[Re, Im, Re, Im...]`.
 /// `data.len()` and `block_size` must both be powers of 2.
-pub fn interleave_inplace<T: Copy + Default>(data: &mut [T], block_size: usize) {
+pub fn interleave_inplace<T: Copy + Default + Send>(data: &mut [T], block_size: usize) {
     let len = data.len();
     assert!(len.is_power_of_two(), "Data length must be a power of 2");
     assert!(
@@ -176,17 +176,34 @@ pub fn interleave_inplace<T: Copy + Default>(data: &mut [T], block_size: usize) 
     }
 
     // Phase 2: Local SIMD Interleave
-    interleave_blocks_inplace(data, block_size)
+    #[cfg(not(feature = "parallel"))]
+    interleave_blocks_inplace(data, block_size);
+    #[cfg(feature = "parallel")]
+    interleave_blocks_inplace_parallel(data, block_size);
 }
 
 /// Phase 2 of in-place block-based interleaving. Single-threaded.
 fn interleave_blocks_inplace<T: Copy + Default>(data: &mut [T], block_size: usize) {
     let mut temp_pair = vec![T::default(); 2 * block_size];
-    for window in data.chunks_exact_mut(2 * block_size) {
-        let (reals, imags) = window.split_at(block_size);
+    for chunk in data.chunks_exact_mut(2 * block_size) {
+        let (reals, imags) = chunk.split_at(block_size);
         combine_re_im_into(reals, imags, &mut temp_pair);
-        window.copy_from_slice(&temp_pair);
+        chunk.copy_from_slice(&temp_pair);
     }
+}
+
+#[cfg(feature = "parallel")]
+/// Phase 2 of in-place block-based interleaving. Multi-threaded through Rayon.
+fn interleave_blocks_inplace_parallel<T: Copy + Default + Send>(data: &mut [T], block_size: usize) {
+    use rayon::prelude::*;
+    data.par_chunks_exact_mut(2 * block_size).for_each_init(
+        || vec![T::default(); 2 * block_size],
+        |temp_pair, chunk| {
+            let (reals, imags) = chunk.split_at(block_size);
+            combine_re_im_into(reals, imags, temp_pair);
+            chunk.copy_from_slice(&temp_pair);
+        },
+    );
 }
 
 /// Reverses the in-place interleaving, returning `[Re, Im, ...]` back to `[Reals | Imags]`.
